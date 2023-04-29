cf55bb39843567b90c0381d494d6034d4aec4d79
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-04-29 11:38:39 +0100

    getall for category- router done

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,10 +2,17 @@ const router = require('express').Router();
 const { Category, Product } = require('../../../models');
 
 // The `/api/categories` endpoint
-
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
 });
 
 router.get('/:id', (req, res) => {
