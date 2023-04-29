1b5d048d6cf40e419d0b704eaa6a82006d3b5a39
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-04-29 11:56:11 +0100

    delete route for product completed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -124,6 +124,17 @@ router.delete('/:id', (req, res) => {
       id: req.params.id,
     },
   })
+    .then((productData) => {
+      if (!productData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(productData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 module.exports = router;
