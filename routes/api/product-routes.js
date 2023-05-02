dd81c507b7667d21ca8aa23648e50112a4d91001
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-05-02 12:21:03 +0100

    fixed products synthax errors

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -37,30 +37,7 @@ router.get('/', (req, res) => {
     console.log(err);
     res.status(500).json(err);
   })
-});
-
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  Product.findOne({
-    where: {
-      id: req.params.id
-      },
-    include: [Category, 
-      {
-        model: Tag,
-        through: ProductTag,
-      }],
-
-  })
-  // be sure to include its associated Category and Tag data
-  .then((productData) => res.json(productData))
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  })
-});
-
+});   
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
